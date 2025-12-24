035bb1ef83b9baacaa9a981558ce158043834f6a
nessanovianti <nessanovianti05@gmail.com>
2025-12-24 22:37:35 +0700

    Fix allowedHosts in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,          // Sesuaikan dengan port Railway
+    allowedHosts: true,  // IZINKAN semua domain (Solusi Error Blocked Request)
+    host: true           // Buka akses network (0.0.0.0)
+  },
+  server: {
+    port: 3000,          // Opsional: Samakan port saat mode develop
+  }
 })
